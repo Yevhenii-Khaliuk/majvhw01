7db5da14ed38318dc7aa87087a085d50ea3406b1
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2019-02-13 14:35:21 +0200

    Refactor: use assertArrayEquals() method, use class reference for static methods calls

--- src/test/java/com/khaliuk/RangesTest.java
+++ src/test/java/com/khaliuk/RangesTest.java
@@ -2,37 +2,29 @@ package com.khaliuk;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class RangesTest {
-    Ranges ranges;
-
-    @Before
-    public void setUp() {
-        ranges = new Ranges();
-    }
-
     @Test
     public void testArrayRangesCorrectForDifferentArgumentsWithFirstAndLastTwoElementsBelongToDifferentRanges() {
         String expectedResult = "[1][3 5][7 8][10 14][16 19][22]";
-        String actualResult = ranges.arrayRanges(new int[] {1, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 16, 17, 18, 19, 22});
+        String actualResult = Ranges.arrayRanges(new int[] {1, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 16, 17, 18, 19, 22});
         assertEquals(expectedResult, actualResult);
     }
     @Test
     public void testArrayRangesCorrectForFirstAndLastTwoElementsBelongToSameRanges() {
         String expectedResult = "[1 5][7 8][10 14][16 20]";
-        String actualResult = ranges.arrayRanges(new int[] {1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20});
+        String actualResult = Ranges.arrayRanges(new int[] {1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20});
         assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testArrayRangesCorrectForSingleElementArray() {
         String expectedResult = "[1]";
-        String actualResult = ranges.arrayRanges(new int[] {1});
+        String actualResult = Ranges.arrayRanges(new int[] {1});
         assertEquals(expectedResult, actualResult);
     }
 }
--- src/test/java/com/khaliuk/RestoreArrayTest.java
+++ src/test/java/com/khaliuk/RestoreArrayTest.java
@@ -1,26 +1,17 @@
 package com.khaliuk;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
-import java.util.Arrays;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class RestoreArrayTest {
-    RestoreArray restore;
-
-    @Before
-    public void setUp() {
-        restore = new RestoreArray();
-    }
-
     @Test
     public void testRestoreArrayReturnsProperResult() {
         int[] expectedResult = new int[] {1, 2, 3, 4, 5, 6};
-        int[] actualResult = restore.restoreArray(new int[] {1, -1, 3, 4, -2, 6});
-        assertTrue(Arrays.equals(expectedResult, actualResult));
+        int[] actualResult = RestoreArray.restoreArray(new int[] {1, -1, 3, 4, -2, 6});
+        assertArrayEquals(expectedResult, actualResult);
     }
 }
